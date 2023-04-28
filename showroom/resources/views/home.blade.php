075271e6ce9a29f971babffd0260a58041f66251
DanZziel <tducournau50340@gmail.com>
2023-04-28 16:51:37 +0200

    home view

--- showroom/resources/views/home.blade.php
+++ showroom/resources/views/home.blade.php
@@ -1,1 +1,40 @@
-<h1>SALUT !</h1>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/main.css" type="text/css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <title>Rave Emblem</title>
+    <title>Document</title>
+</head>
+<body>
+    <div class="row bg-dark pt-3">
+        <div class="col-3 mt-2 ms-4">
+            <img src="assets/images/Logo.png" width="320" height="80">
+        </div>
+        <div class="col-3 mt-2 ms-2 text-white text-end">
+            <p>Se connecter</p>
+        </div> 
+        <div class="col-3 mt-2 ms-5 text-white text-end">
+            <p>Tous les concerts</p>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="d-flex justify-content-center">
+        <div class="row ms-1">
+            <div class="col ms-5">
+                <img src="assets/images/mainpicture.jpg" width="640" height="450">
+            </div>
+            <div class="col mt-4">
+                <h2>CONCERT ROCK</h2>
+                <button>Réserver</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
