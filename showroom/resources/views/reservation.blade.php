8a3fbdadc8c51045ec3227b9d86734f498617806
DanZziel <tducournau50340@gmail.com>
2023-05-03 13:42:43 +0200

    viewpicture

--- showroom/resources/views/reservation.blade.php
+++ showroom/resources/views/reservation.blade.php
@@ -2,7 +2,7 @@
 <div class="card mb-3" style="max-width: 1500px;">
     <div class="row g-0">
       <div class="col-md-4">
-        <img src="assets/images/mainpicture.jpg" class="img-fluid rounded-start" alt="...">
+        <img src="{{$concert->imgartiste}}" class="img-fluid rounded-start" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
